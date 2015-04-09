a8d644d4cadcb7d52dc40223b71294258dc5f4ab
Harm Aarts <harmaarts@gmail.com>
2015-04-09 20:08:40 +0200

    Point to correct JSONs.

--- cmd/conjoiner/main.go
+++ cmd/conjoiner/main.go
@@ -209,7 +209,7 @@ func (c conjoiner) showFunc(show show) filepath.WalkFunc {
 	return func(dir string, info os.FileInfo, err error) error {
 		if c.isShowRoot(dir) {
 			for i, season := range show.seasons {
-				location := path.Join(dir, strconv.Itoa(season.Number)+".json")
+				location := path.Join(dir, strconv.Itoa(season.Number), "episodes.json")
 				show.seasons[i].URL = withoutRoot(c.root, location)
 				err := writeObject(season, location)
 				if err != nil {
@@ -303,7 +303,7 @@ func (c conjoiner) createJSONs(shows map[os.FileInfo]show) error {
 
 	var showIndex []show
 	for _, show := range shows {
-		URL := show.Title + ".json"
+		URL := path.Join(show.Title,  "seasons.json")
 		show.URL = URL
 
 		err := writeObject(show, path.Join(c.root, URL))
