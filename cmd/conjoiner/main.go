e807d22e55471846a123adcd17d10b4f6d50d4f1
Harm Aarts <harmaarts@gmail.com>
2015-09-14 12:21:34 +0200

    Split web client from main.
    Consider this the first move in order to ditch the slow and unreliable
    Trakt API. TvMaze looks promising.

--- cmd/conjoiner/main.go
+++ cmd/conjoiner/main.go
@@ -82,30 +82,6 @@ func (c conjoiner) listShows() []os.FileInfo {
 	return shows
 }
 
-type Trakt struct {
-	*trakt.Client
-}
-
-type episode struct {
-	trakt.Episode
-	URL      string `json:"url"` // Useful when having a list of episodes and you want the single episode.
-	VideoURL string `json:"video_url"`
-}
-
-type season struct {
-	trakt.Season
-	episodes    []episode
-	URL         string `json:"url"` // Useful when season is presented in a list.
-	EpisodesURL string `json:"episodes_url"`
-}
-
-type show struct {
-	trakt.Show
-	seasons    []season
-	URL        string `json:"url"` // Useful when show is presented in a list.
-	SeasonsURL string `json:"seasons_url"`
-}
-
 func retry(f func() error) error {
 	var err error
 	for i := 0; i < 3; i++ {
@@ -115,71 +91,6 @@ func retry(f func() error) error {
 	}
 
 	return err
-}
-
-func (t Trakt) turnDirsIntoShows(dirs []os.FileInfo) map[os.FileInfo]trakt.ShowResult {
-	shows := make(map[os.FileInfo]trakt.ShowResult)
-
-	for _, d := range dirs {
-		var results []trakt.ShowResult
-		var response *trakt.Result
-		operation := func() error {
-			showName := strings.Replace(path.Base(d.Name()), " (US)", "", 1) //RLY? Trakt is very broken.
-			results, response = t.Shows().Search(showName)
-			return response.Err
-		}
-		retry(operation)
-
-		if len(results) > 0 {
-			shows[d] = results[0]
-		}
-	}
-
-	return shows
-}
-
-func (t Trakt) turnShowResultsIntoShows(showResults map[os.FileInfo]trakt.ShowResult) map[os.FileInfo]show {
-	shows := make(map[os.FileInfo]show)
-
-	for dir, s := range showResults {
-		result, response := t.Shows().One(s.Show.IDs.Trakt)
-		if response.Err != nil {
-			continue
-		}
-
-		shows[dir] = show{Show: *result}
-	}
-
-	return shows
-}
-
-func (t Trakt) addSeasonsAndEpisodesToShows(shows map[os.FileInfo]show) {
-	for k, show := range shows {
-		t.addSeasons(&show)
-		t.addEpisodes(&show)
-		shows[k] = show
-	}
-}
-
-func (t Trakt) addSeasons(show *show) {
-	seasons, response := t.Seasons().All(show.IDs.Trakt)
-	if response.Err == nil {
-		for _, s := range seasons {
-			show.seasons = append(show.seasons, season{Season: s}) // Wow this is really weird obmitting the package name.
-		}
-	}
-}
-
-func (t Trakt) addEpisodes(show *show) {
-	for k, season := range show.seasons {
-		episodes, response := t.Episodes().AllBySeason(show.IDs.Trakt, season.Number)
-		if response.Err == nil {
-			for _, e := range episodes {
-				season.episodes = append(season.episodes, episode{Episode: e})
-			}
-		}
-		show.seasons[k] = season
-	}
 }
 
 func (c conjoiner) lookup() map[os.FileInfo]show {
@@ -214,16 +125,6 @@ func writeObject(v interface{}, file string) error {
 	}
 
 	return nil
-}
-
-func (s show) findSeason(number int) (season, error) {
-	for _, season := range s.seasons {
-		if season.Number == number {
-			return season, nil
-		}
-	}
-
-	return season{}, fmt.Errorf("Could not find season %d of %s", number, s.Title)
 }
 
 func withoutRoot(root, path string) string {
