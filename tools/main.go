d202496e0a8a41afc7304f9c767ec7f4ff4f4bca
Harm Aarts <harmaarts@gmail.com>
2017-01-04 11:43:29 +0100

    tools: serve the example result too
    This can be used for developing the apps locally.

--- tools/main.go
+++ tools/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-  "net/http"
-  "net/http/httputil"
-  "net/url"
-  "fmt"
-  "log"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
-  fmt.Println("This server serves files located in '../static'")
+	fmt.Println("Listening on localhost:8081")
+	fmt.Println("'/' serves files located in '../static'")
+	fmt.Println("'/shows/' serves files located in '../cmd/fetcher/testdata/example_result/shows'")
+	fmt.Println("'/login' and '/register' proxy to authme (which should run on localhost:8080)")
+
 	http.Handle("/", http.FileServer(http.Dir("../static")))
+	http.Handle("/shows/", http.FileServer(http.Dir("../cmd/fetcher/testdata/example_result/")))
 
 	rpURL, err := url.Parse("http://localhost:8080")
 	if err != nil {
-  	    log.Fatal(err)
+		log.Fatal(err)
 	}
 	http.Handle("/login", httputil.NewSingleHostReverseProxy(rpURL))
 	http.Handle("/register", httputil.NewSingleHostReverseProxy(rpURL))
