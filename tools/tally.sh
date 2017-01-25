f62dc112c33be05169c3db5d4e2003eba8821388
Harm Aarts <harmaarts@gmail.com>
2017-01-25 23:27:38 +0100

    tally: more compact
    As the number of shows grows.

--- tools/tally.sh
+++ tools/tally.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-find Public -maxdepth 1 -type d -print0 | while read -d $'\0' dir
+find Public -maxdepth 1 -type d -print0 | sort -z | while read -d $'\0' dir
 do
         if [ "$dir" != "Public" ]; then
                 converted=$(find "$dir" -name "*webm" | wc -l)
                 unconverted=$(find "$dir" -type l -name '*avi' -o -name '*mp4' -o -name '*mkv' | wc -l)
 
-                echo "$dir"
-                echo "unconverted: $unconverted"
-                echo "converted:   $converted"
+                dir=$(echo $dir | sed 's/Public\///')
+                printf "%3d/%-3d : %s\n" $converted $unconverted "$dir"
         fi
 done
 
