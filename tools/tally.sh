38c171fb4c427f2e061f6e7640c492ccbb3f059a
Harm Aarts <harmaarts@gmail.com>
2017-04-22 11:22:27 +0200

    Add 'available' column

--- tools/tally.sh
+++ tools/tally.sh
@@ -6,8 +6,11 @@ do
                 converted=$(find "$dir" -name "*webm" | wc -l)
                 unconverted=$(find "$dir" -type l -name '*avi' -o -name '*mp4' -o -name '*mkv' | wc -l)
 
-                dir=$(echo $dir | sed 's/Public\///')
-                printf "%3d/%-3d : %s\n" $converted $unconverted "$dir"
+                showname=$(echo $dir | sed 's/Public\///')
+                showname_without_year=$(echo $showname | sed -E 's/ \([[:digit:]]+\)//' | sed 's/ /%20/g')
+                available=$(curl --silent "http://api.tvmaze.com/singlesearch/shows?q=$showname_without_year&embed=episodes" | jq '._embedded.episodes | length')
+                # '%-3d' means: line out to the left
+                printf "%3d/%3d/%3d : %s\n" $converted $unconverted $available "$showname"
         fi
 done
 
